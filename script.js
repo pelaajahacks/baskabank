bb3e900af831a30b50effa3d12423ef32e6e6c94
pelaajahacks <62875333+pelaajahacks@users.noreply.github.com>
2022-01-28 11:56:10 +0200

    free corona

--- script.js
+++
@@ -1,3 +0,0 @@
-var _0xa2a2=["\x6C\x6F\x67\x69\x6E","\x72\x65\x70\x6C\x61\x63\x65","\x6C\x6F\x63\x61\x74\x69\x6F\x6E"];window[_0xa2a2[2]][_0xa2a2[1]](_0xa2a2[0])
-
-//https://www.javascriptobfuscator.com/Javascript-Obfuscator.aspx
