3a332d6b75570fdb9da2962b3a0580d975902862
pelaajahacks <62875333+pelaajahacks@users.noreply.github.com>
2022-03-29 09:13:28 +0300

    Update login.js

--- baskabank/login/login.js
+++ baskabank/login/login.js
@@ -2,7 +2,7 @@ function validate() {
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
 
-    if(username=="aki") {
+    if(username=="zwiebel") {
         document.cookie = "logged_in=true";
         window.location.replace("/baskabank/logged_in");   
         return false;
